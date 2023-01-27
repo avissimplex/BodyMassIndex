2ec1a7b7631a9498a0764fd09d3641455ea35efc
Ksenya Sunduova <avissimplex@gmail.com>
2023-01-27 22:42:17 +0400

    Humiliating diagnosis

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,13 @@
 public class BmiService {
     public float calculate(int mass, float height) {
         float result = mass / (height * height);
+        if (result > 25) {
+            System.out.println("ты жирная");
 
+        } else {
+            System.out.println("ты тощая");
+        }
         return result;
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,15 @@ public class Main {
         int mass = 73;
         float height = 1.72F;
         float index = service.calculate(mass, height);
-        
+        System.out.println("масса=73 вес=1,27");
         System.out.println(index);
+
+        System.out.println();
+
+        int mass1 = 100;
+        float height1 = 1.50F;
+        float index1 = service.calculate(mass1, height1);
+        System.out.println("масса=100 вес=1,50");
+        System.out.println(index1);
     }
 }
